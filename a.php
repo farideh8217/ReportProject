204e1217f7a17a21e716d3101a40fa9d5204aea5
Faride Zaker <farideh.zaker8217@yahoo.com>
2022-08-06 11:19:16 +0430

    report

--- a.php
+++ a.php
@@ -1,5 +1,29 @@
 <?php
 require "load.php";
+
+if (isset($_POST["submitreport"])) {
+
+    $dateofday = $_POST["dateofday"];
+    $hourslogin = $_POST["hourslogin"];
+    $minutelogin = $_POST["minutelogin"];
+    $hourslogout = $_POST["hourslogout"];
+    $minutelogout = $_POST["minutelogout"];
+    $reporttext = $_POST["reporttext"];
+
+    $sql = "INSERT INTO `report` SET dateofday= :dateofday, hourslogin= :hourslogin, 
+    minutelogin= :minutelogin, hourslogout= :hourslogout, minutelogout= :minutelogout, reporttext= :reporttext";
+    $stmt = $db->prepare($sql);
+    $stmt->bindvalue(":dateofday",$dateofday);
+    $stmt->bindvalue(":hourslogin",$hourslogin);
+    $stmt->bindvalue(":minutelogin",$minutelogin);
+    $stmt->bindvalue(":hourslogout",$hourslogout);
+    $stmt->bindvalue(":minutelogout",$minutelogout);
+    $stmt->bindvalue(":reporttext",$reporttext);
+    $stmt->execute();
+
+    header("Location: b.php");
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,33 +35,43 @@ require "load.php";
     <title>Document</title>
 </head>
 <body>
+<form action="" method="POST">
     <h1>فرم ثبت گزارش روزانه</h1>
     <div class="report">
         <p class="date">تاریخ</p><br>
-        <p class="date">24/5/1400<p><br>
+        <input type="date" name="dateofday"><br><br>
         <h3>ساعت ورود:</h3>
-        <select>
-            <option>8</option>
+        <select name="hourslogin">
+            <?php for($i = 8; $i<=14; $i++){ ?>
+                <option value="<?= $i ?>"<?php if ($i==8) echo" selected" ?>> <?= $i ?></option>
+            <?php } ?>
         </select><br>    
         <h3>دقیقه ورود:</h3>
-        <select>
-            <option>8</option>
+        <select name="minutelogin">
+            <?php for($i = 0; $i<=55; $i += 5){ ?>
+                <option value="<?= $i ?>"><?= $i ?></option>
+            <?php } ?>
         </select><br><br><br>          
         <h3>ساعت خروج:</h3>
-        <select>
-            <option>14</option>
-        </select><br>       
+        <select name="hourslogout">
+            <?php for($i = 8; $i<=14; $i++){ ?>
+                <option value="<?= $i ?>"<?php if ($i==14) echo" selected" ?>> <?= $i ?></option>
+            <?php } ?>
+        </select><br>    
         <h3>دقیقه خروج:</h3>
-        <select>
-            <option>8</option>
-        </select> <br>   
+        <select name="minutelogout">
+            <?php for($i = 0; $i<=55; $i += 5){ ?>
+                <option value="<?= $i ?>"><?= $i ?></option>
+            <?php } ?>
+        </select>
         
         <div class="text">
         <h3>گزارش خود را در این قسمت بنویسید</h3>
-            <textarea></textarea>
+            <textarea name="reporttext"></textarea>
         </div> 
-        
-        <input type="submit" value="ثبت گزارش" name="submitreport">
+        <input class="submitreport" type="submit" value="ثبت گزارش" name="submitreport">
     </div>
+
+</form>    
 </body>
 </html>
---
+++ report.php
@@ -0,0 +1,7 @@
+<?php
+require "load.php";
+
+
+
+
+?>
